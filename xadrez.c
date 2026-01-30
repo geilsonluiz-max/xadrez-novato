84d72913c0371f28d27a4f8d0436ad080829f9ab
Geilsonluiz-max <geilson.luiz@gmail.com>
2026-01-30 15:09:55 -0300

    Simulacao dos movimentos da Torre, Bispo e Rainha - nivel novato

---
+++ xadrez.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+	int main() {
+
+/*Movimento da torre, estrutura usada: FOR. A torre vai andar 5 casas para a direita */
+
+		int casas_torre = 5;
+
+		printf("Movimento da torre:\n");
+
+		for (int i = 1; i <= casas_torre; i++) {
+			printf("Direita\n");
+		}
+
+		printf("\n");
+
+/*Movimento do bispo, estrutura usada: WHILE. O bispo vai andar 5 casas na diagonal (Cima + Direita) */
+
+	int casas_bispo = 5;
+	int contador_bispo = 1;
+
+	printf("Movimento do Bispo:\n");
+
+	while (contador_bispo <= casas_bispo) {
+		printf("Cima Direita\n");
+		contador_bispo++;
+	}
+
+	printf("\n");
+
+/*Movimento da rainha, estrutura usada: DO-WHILE. A rainha vai andar 8 casas para a esquerda */
+
+	int casas_rainha = 8;
+	int contador_rainha = 1;
+
+	printf("Movimento da Rainha:\n");
+
+	do {
+		printf("Esquerda\n");
+		contador_rainha++;
+	} while (contador_rainha <= casas_rainha);
+
+	return 0;
+}
